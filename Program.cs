cc54000300e7244330a78dc31664d2d32fb7f942
Forogtaff123 <85927323+Forogtaff123@users.noreply.github.com>
2023-11-28 19:17:18 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stimulsoft.Report;
+
+namespace StimulSoftTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the report object
+            var report = new StiReport();
+
+            // Load the report
+            report.Load("MyReport.mrt");
+            try
+            {
+                // Render the report
+                report.Render();
+
+                // Print the report
+                report.Print();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+                throw;
+            }
+
+        }
+    }
+}
